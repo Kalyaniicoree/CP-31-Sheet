bb3a471cf17b7112080c0ab13ae0ebbafdf49a8c
Kalyaniicoree <kalyanigun9@gmail.com>
2026-07-04 22:44:04 +0530

    TC O(1)

---
+++ 900/AB Balance.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void solve(){
+    string s;
+    cin>>s;
+    int n=s.length();
+    if(s[0]==s[n-1]){
+        cout<<s<<endl;
+    }
+    else{
+        s[0]=s[n-1];
+        cout<<s<<endl;
+    }
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
