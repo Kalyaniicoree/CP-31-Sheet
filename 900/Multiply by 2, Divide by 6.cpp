cd1942628899f18178e37e4aacdcb9402c41c154
Kalyaniicoree <kalyanigun9@gmail.com>
2026-07-05 20:37:31 +0530

    TC O(N+M)

---
+++ 900/Multiply by 2, Divide by 6.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+void solve(){
+    long long n;
+    cin>>n;
+    int count2=0;
+    int count3=0;
+    while(n%2==0){
+        n/=2;
+        count2++;
+    }
+    while(n%3==0){
+        n/=3;
+        count3++;
+    }
+    if(n!=1 || count2>count3){
+        cout<<-1<<endl;
+    }
+    else{
+        cout<<(count3-count2)+count3<<endl;
+    }
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
